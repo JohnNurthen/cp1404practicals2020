29b03ab3ac577b865dc9b80e5ade1ba612eb02af
John Nurthen <john.nurthen@my.jcu.edu.au>
2019-08-22 12:18:16 +1000

    Add get_password function

---
+++ prac_03/password_check_with_functions.py
@@ -0,0 +1,19 @@
+'''John Nurthen'''
+min_length = int(3)
+
+
+def main()
+
+
+def get_password()
+    password = input('Please enter your Password: ')
+    password_length = len(password)
+    while password_length < min_length:
+        print('Password must be between 3 and 10 characters')
+    password = input('Please enter a valid Password: ')
+    password_length = len(password)
+
+
+print('*' * password_length)
+
+main()
