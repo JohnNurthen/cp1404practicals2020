48b24b5d419c7b81fae070b980779e5bd4c5129c
JohnNurthen <john.nurthen@my.jcu.edu.au>
2020-03-22 21:38:39 +1000

    Initialise lists_warmup.py

---
+++ cp1404practicals2020/prac_04/lists_warmup.py
@@ -0,0 +1,19 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+"""numbers[0] = 3
+numbers[-1] = 2
+numbers[3] = 1
+numbers[:-1] = 3, 1, 4, 1,5, 9
+numbers[3:4] = 1, 5
+5 in numbers = true
+7 in numbers = false
+"3" in numbers = false
+numbers + [6, 5, 3] = 3, 1, 4, 5, 9, 2, 6, 5, 3"""
+
+numbers[0] = str("ten")
+print(numbers[0])
+numbers[-1] = 1
+print(numbers[-1])
+numbers[2:]
+print(numbers[2:])
+9 in numbers
+print(9 in numbers)
